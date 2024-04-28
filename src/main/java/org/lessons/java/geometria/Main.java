ff79b29cf5a9b1554d80323237b85dab4cf40967
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-04-28 18:46:22 +0200

    Esercizio rivisto OOP

--- src/main/java/org/lessons/java/geometria/Main.java
+++ src/main/java/org/lessons/java/geometria/Main.java
@@ -1,24 +1,28 @@
 package org.lessons.java.geometria;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Scanner input = new Scanner(System.in);
-		
-		Rettangolo dati = new Rettangolo();
-		
-		System.out.println("Inserire la base del rettangolo");
-		dati.base = input.nextInt();
-		
-		System.out.println("Inserire l'altezza del rettangolo");
-		dati.altezza = input.nextInt();
-		
-		dati.StampaDati();
-		
 
+		// Invoco la classe Rettangolo
+		Rettangolo rettangolo = new Rettangolo();
+
+		// Invoco i metodi che ritornano i valori di Area e Perimetro
+		System.out.println("L'area del rettangolo è: " + rettangolo.calcolaArea());
+		System.out.println("Il perimetro del rettangolo è: " + rettangolo.calcolaPerimetro());
+
+		/*
+		 * << METODO 1 >> Scanner input = new Scanner(System.in);
+		 * 
+		 * Rettangolo dati = new Rettangolo();
+		 * 
+		 * System.out.println("Inserire la base del rettangolo"); dati.base =
+		 * input.nextInt();
+		 * 
+		 * System.out.println("Inserire l'altezza del rettangolo"); dati.altezza =
+		 * input.nextInt();
+		 * 
+		 * dati.StampaDati();
+		 */
 	}
-
 }
--- src/main/java/org/lessons/java/geometria/Rettangolo.java
+++ src/main/java/org/lessons/java/geometria/Rettangolo.java
@@ -3,15 +3,39 @@ package org.lessons.java.geometria;
 import java.util.Scanner;
 
 public class Rettangolo {
-	
-	int base;
-	int altezza;
-	
-	public void StampaDati() {
-		int area = base * altezza;
-		int perimetro = (base * 2) + (altezza * 2);
-		System.out.println("L'area del rettangolo è " + area);
-		System.out.println("Il perimetro del rettangolo è " + perimetro);
 
-	 }
+	public int base;
+	public int altezza;
+
+	// Creo un costruttore senza parametri per far inizializzare l'oggetto
+	// direttamente dall'utente con input tastiera.
+	public Rettangolo() {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Inserisci la base del rettangolo");
+		base = scanner.nextInt();
+
+		System.out.println("Inserisci l'altezza del rettangolo");
+		altezza = scanner.nextInt();
+
+		scanner.close();
+	}
+	
+	// Creo un metodo che ritorna il valore di calcolo dell'area
+	public int calcolaArea() {
+		return base * altezza;
+	}
+
+	// Creo un metodo che ritorna il valore di calcolo dell perimetro
+	public int calcolaPerimetro() {
+		return (base * 2) + (altezza + 2);
+	}
+
+	/*
+	 * << METODO 1 >> int base; int altezza;
+	 * 
+	 * public void StampaDati() { int area = base * altezza; int perimetro = (base *
+	 * 2) + (altezza * 2); System.out.println("L'area del rettangolo è " + area);
+	 * System.out.println("Il perimetro del rettangolo è " + perimetro);
+	 */
 }
